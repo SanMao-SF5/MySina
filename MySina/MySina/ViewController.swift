c74dd3980e9498c0034f8fad5790caaf04d23a9d
shenfangwu <shenfangwu@163.com>
2015-12-11 16:15:09 +0800

    创建了项目，添加了ignore文件，做了基本的项目配置

---
+++ MySina/MySina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySina
+//
+//  Created by 沈方武 on 15/12/11.
+//  Copyright © 2015年 SanMao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
